2dcd1197dfed4de29932b408798b20684ae2aa9c
Emilio <emiliogg84@outlook.com>
2024-10-05 10:06:28 -0500

    Create Product.cs

---
+++ src/ProductsAPI/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsAPI.Models;
+
+public class Product : EntityBase
+{
+	public required string Name { get; set; }
+	public string? Details { get; set; }
+	public string Status { get; set; }
+	public double UnitPrice { get; set; }
+}
