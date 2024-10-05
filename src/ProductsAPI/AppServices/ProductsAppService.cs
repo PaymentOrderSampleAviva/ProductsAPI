a306ff719e143bb935cfbe91643cad948cc7f6d0
Emilio <emiliogg84@outlook.com>
2024-10-05 10:43:01 -0500

    Add ProductAppService

---
+++ src/ProductsAPI/AppServices/ProductsAppService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProductsAPI.DataContracts;
+using ProductsAPI.Repositories;
+
+namespace ProductsAPI.AppServices;
+
+public class ProductsAppService(IProductsRepository repository, IMapper mapper, Logger<ProductsAppService> logger) : IProductsAppService 
+{
+	private readonly IProductsRepository _repository = repository;
+	private readonly IMapper _mapper = mapper;
+	private readonly ILogger _logger = logger;
+
+	public async Task<IReadOnlyList<ProductResponse>> ListAllAsync()
+	{
+		var products = await _repository.GetAllAsync();
+		return _mapper.Map<List<ProductResponse>>(products);
+	}
+}
---
+++ src/ProductsAPI/DataContracts/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿using ProductsAPI.Models;
+
+namespace ProductsAPI.DataContracts;
+
+public class ProductResponse
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public string? Details { get; set; }
+	public required string Status { get; set; }
+	public double UnitPrice { get; set; } = 0;
+}
