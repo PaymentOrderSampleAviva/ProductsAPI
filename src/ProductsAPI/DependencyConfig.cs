c87adccfc5f6adf1c1286585b02f69666a7cf7b5
Emilio <emiliogg84@outlook.com>
2024-10-05 11:12:42 -0500

    Create DependencyConfig.cs

---
+++ src/ProductsAPI/DependencyConfig.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.AppServices;
+using ProductsAPI.Data;
+using ProductsAPI.Repositories;
+using System.Reflection;
+
+namespace ProductsAPI;
+
+public static class DependencyConfig
+{
+	public static IServiceCollection AddCoreDependenciesDev(this IServiceCollection services)
+	{
+		services.AddCoreDependencies();
+		services.AddInMemoryDataStorage();
+		return services;
+	}
+
+	public static IServiceCollection AddCoreDependenciesPro(this IServiceCollection services)
+	{
+		services.AddCoreDependencies();
+		//Register production data storage here.
+		return services;
+	}
+
+	public static IServiceCollection AddCoreDependencies(this IServiceCollection services)
+	{
+		services.AddAppServices();
+		services.AddRepositories();
+		services.AddDefaultServices();
+		return services;
+	}
+
+	private static IServiceCollection AddAppServices(this IServiceCollection services)
+	{
+		services.AddTransient<IProductsAppService, ProductsAppService>();
+		return services;
+	}
+
+	private static IServiceCollection AddRepositories(this IServiceCollection services)
+	{
+		services.AddTransient<IProductsRepository, ProductsRepository>();
+		return services;
+	}
+
+	private static IServiceCollection AddInMemoryDataStorage(this IServiceCollection services)
+	{
+		services.AddDbContext<ApplicationDbContext>(options => {
+			options.UseInMemoryDatabase("ProductsDb-Dev");
+		});
+		return services;
+	}
+
+	private static IServiceCollection AddDefaultServices(this IServiceCollection services)
+	{
+		services.AddAutoMapper(Assembly.GetExecutingAssembly());
+		return services;
+	}
+}
