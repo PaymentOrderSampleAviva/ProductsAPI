0dc5a51c2a3f57dee4b27127f0ff3f8955378902
Emilio <emiliogg84@outlook.com>
2024-10-05 10:11:03 -0500

    Update Product.cs

--- src/ProductsAPI/Models/Product.cs
+++ src/ProductsAPI/Models/Product.cs
@@ -5,5 +5,5 @@ public class Product : EntityBase
 	public required string Name { get; set; }
 	public string? Details { get; set; }
 	public ProductStatus Status { get; set; } = ProductStatus.NotAvailable;
-	public double UnitPrice { get; set; }
+	public double UnitPrice { get; set; } = 0;
 }
