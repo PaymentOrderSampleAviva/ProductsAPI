e05ce26a7cfaf04d6e0b2ce1f3b01400dd9803a4
Emilio <emiliogg84@outlook.com>
2024-10-05 11:32:40 -0500

    Create ProductsController.cs

---
+++ src/ProductsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.AppServices;
+using ProductsAPI.DataContracts;
+
+namespace ProductsAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController(IProductsAppService productsAppService, ILogger<ProductsController> logger) : ControllerBase
+{
+	private readonly IProductsAppService _productsAppService = productsAppService;
+	private readonly ILogger<ProductsController> _logger = logger;
+
+	[HttpGet]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	[ProducesDefaultResponseType(typeof(List<ProductResponse>))]
+	public async Task<IActionResult> GetProductsAsync()
+	{
+		try
+		{
+			var result = await _productsAppService.ListAllAsync();
+			return Ok(result);
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(ex, "An exception was thrown getting the product list.");
+		}
+
+		return StatusCode(500);
+	}
+}
