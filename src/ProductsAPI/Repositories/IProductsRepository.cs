78a9aa641ec27dfee423e245cf1b2f56112bc861
Emilio <emiliogg84@outlook.com>
2024-10-07 09:06:13 -0500

    Add GetAvailableCountAsync() method to ProductsRepo

--- src/ProductsAPI/Repositories/IProductsRepository.cs
+++ src/ProductsAPI/Repositories/IProductsRepository.cs
@@ -5,5 +5,6 @@ namespace ProductsAPI.Repositories
 	public interface IProductsRepository
 	{
 		Task<IReadOnlyList<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+		Task<int> GetAvailableCountAsync(List<int> productIds, CancellationToken cancellationToken = default);
 	}
 }
