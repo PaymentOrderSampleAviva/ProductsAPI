b3bbfc126233f5b7c2e487218991bf4371e94049
Emilio <emiliogg84@outlook.com>
2024-10-07 01:40:46 -0500

    use constructor vars in payment processors

--- src/ProductsAPI/PaymentProcessors/PaymentProcessorSelector.cs
+++ src/ProductsAPI/PaymentProcessors/PaymentProcessorSelector.cs
@@ -6,21 +6,18 @@ namespace ProductsAPI.PaymentProcessors;
 public class PaymentProcessorSelector(IEnumerable<FeeProvider> feeProviders, IServiceProvider serviceProvider)
 	: IPaymentProcessorSelector
 {
-	private readonly IEnumerable<FeeProvider> _feeProviders = feeProviders;
-	private readonly IServiceProvider _serviceProvider = serviceProvider;
-
 	public IPaymentProcessor? Select(double totalAmount)
 	{
 		var processorType = GetLowerFeeProcessor(totalAmount);
 
 		if (processorType == null) throw new ArgumentException("Invalid processor type");
 
-		return _serviceProvider.GetService(processorType) as IPaymentProcessor;
+		return serviceProvider.GetService(processorType) as IPaymentProcessor;
 	}
 
 	private Type? GetLowerFeeProcessor(double totalAmount)
 	{
-		var type = _feeProviders
+		var type = feeProviders
 			.Where(x => x.MinAmount < totalAmount)
 			.OrderBy(x => x.FeePercent)
 			.Select(x => x.ProcessorType)
