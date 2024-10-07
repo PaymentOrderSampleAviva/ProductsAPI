73217d5bec7ac81fce04b432a0c7247ae5bd8825
Emilio <emiliogg84@outlook.com>
2024-10-07 09:03:23 -0500

    check products availability

--- src/ProductsAPI.Application/OrdersAppService.cs
+++ src/ProductsAPI.Application/OrdersAppService.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace ProductsAPI.Application;
 
 public class OrdersAppService(IOrdersRepository ordersRepository,
+    IProductsRepository productsRepository,
     IPaymentMethodSelector paymentMethodSelector,
     IMapper mapper, ILogger<OrdersAppService> logger) : IOrdersAppService
 {
@@ -24,6 +25,12 @@ public class OrdersAppService(IOrdersRepository ordersRepository,
     {
         request.ThrowIfNull();
         request.Products.Throw().IfCountLessThan(1);
+
+        // check product availability
+        var productIDs = request.Products.Select(r => r.ProductId).ToList();
+        var availableProductsCount = await productsRepository.GetAvailableCountAsync(productIDs);
+
+		productIDs.Throw(() => new ArgumentException("The product list should not contain not available products")).IfCountGreaterThan(availableProductsCount);
 
         // Save order to db
         var orderEntity = mapper.Map<Order>(request);
